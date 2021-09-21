94ea8916ced76d54dec6099955509d0843a1e52f
Brandon K. Waltz <bwaltz6@gmail.com>
2021-09-21 14:16:55 -0400

    script

--- scripts/assign_story_type.php
+++ scripts/assign_story_type.php
@@ -19,11 +19,22 @@ if ($conn->connect_error) {
 // FA
 $idsFA = $conn->query("select p.ID from wp_posts p inner join wp_term_relationships tr on p.ID = tr.object_id where tr.term_taxonomy_id = 20 and p.post_type = 'external_post' and post_status='publish'");
 
-while ($row = $idsFA->fetch_assoc())
+while ($rowFA = $idsFA->fetch_assoc())
 {
-    die(var_dump($row));
+    //498
+    $conn->query("INSERT INTO wp_term_relationships (object_id, term_taxonomy_id, term_order) VALUES ({$rowFA['ID']}, 498, 0)");
+    $conn->query("DELETE FROM wp_term_relationships WHERE object_id={$rowFA['ID']}");
 }
 
 
 // ITN
 $idsITN = $conn->query("select p.ID from wp_posts p inner join wp_term_relationships tr on p.ID = tr.object_id where tr.term_taxonomy_id = 195 and p.post_type = 'external_post' and post_status='publish'");
+
+while ($rowITN = $idsITN->fetch_assoc())
+{
+    //497
+    $conn->query("INSERT INTO wp_term_relationships (object_id, term_taxonomy_id, term_order) VALUES ({$rowITN['ID']}, 497, 0)");
+    $conn->query("DELETE FROM wp_term_relationships WHERE object_id={$rowITN['ID']}");
+}
+
+$conn->close();

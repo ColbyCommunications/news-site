5e3d0541d5a7692eafdf863d61b9eb6b30229b87
Brandon K. Waltz <bwaltz6@gmail.com>
2022-04-05 10:24:26 -0400

    crons

--- wp-config.php
+++ wp-config.php
@@ -147,7 +147,7 @@ if (false !== $strRelationships = getenv('PLATFORM_RELATIONSHIPS')) {
 // Define wp-content directory outside of WordPress core directory
 define('WP_HOME', $site_scheme . '://' . $site_host);
 define('WP_SITEURL', WP_HOME . '/wp');
-
+define('DISABLE_WP_CRON', true);
 define('WP_CONTENT_DIR', dirname(__FILE__) . '/web/wp-content');
 
 $strContentURL =  WP_HOME . '/wp-content';

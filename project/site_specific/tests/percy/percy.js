a23fd1aac8211e1d7ced8fa375a71349840966a9
Brandon K. Waltz <bwaltz6@gmail.com>
2025-09-22 16:41:58 -0400

    Update percy.js

--- project/site_specific/tests/percy/percy.js
+++ project/site_specific/tests/percy/percy.js
@@ -2,112 +2,111 @@ const puppeteer = require('puppeteer');
 const percySnapshot = require('@percy/puppeteer');
 const scrollToBottom = require('scroll-to-bottomjs');
 
+let site = execSync('~/.platformsh/bin/platform environment:info edge_hostname');
+let siteFull = `https://${site}`;
+
 (async () => {
-  const browser = await puppeteer.launch({ headless: true, args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-  ]});
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
 
-  const scrollOptions = {
-    frequency: 100,
-    timing: 200, // milliseconds
-  };
+    const scrollOptions = {
+        frequency: 100,
+        timing: 200, // milliseconds
+    };
 
-  // Test Page
-  const testPage = await browser.newPage();
-  await testPage.goto('https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/story/colby-announces-recent-faculty-promotions/');
-  await new Promise(function (resolve) {
-    setTimeout(async function  () {
-      await testPage.evaluate(scrollToBottom, scrollOptions);
+    // Test Page
+    const testPage = await browser.newPage();
+    await testPage.goto(`${siteFull}/story/colby-announces-recent-faculty-promotions/`);
+    await new Promise(function (resolve) {
+        setTimeout(async function () {
+            await testPage.evaluate(scrollToBottom, scrollOptions);
 
-      await percySnapshot(testPage, 'Snapshot of test page', {
-        percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
-      });
-      resolve();
-    }, 3000);
-  });
+            await percySnapshot(testPage, 'Snapshot of test page', {
+                percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
+            });
+            resolve();
+        }, 3000);
+    });
 
-  // Test Page 2
-  const testPage2 = await browser.newPage();
-  await testPage2.goto(
-    'https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/story/scholarship-stories-about-colby-faculty-in-2023/'
-  );
-  await new Promise(function (resolve) {
-    setTimeout(async function  () {
-      await testPage2.evaluate(scrollToBottom, scrollOptions);
+    // Test Page 2
+    const testPage2 = await browser.newPage();
+    await testPage2.goto(`${siteFull}/story/scholarship-stories-about-colby-faculty-in-2023/`);
+    await new Promise(function (resolve) {
+        setTimeout(async function () {
+            await testPage2.evaluate(scrollToBottom, scrollOptions);
 
-      await percySnapshot(testPage2, 'Snapshot of test page 2', {
-        percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
-      });
-      resolve();
-    }, 3000);
-  });
+            await percySnapshot(testPage2, 'Snapshot of test page 2', {
+                percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
+            });
+            resolve();
+        }, 3000);
+    });
 
-  // Test Page 3
-  const testPage3 = await browser.newPage();
-  await testPage3.goto(
-    'https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/story/an-adrenaline-junkie-with-a-passion-for-filmmaking/'
-  );
-  await new Promise(function (resolve) {
-    setTimeout(async function  () {
-      await testPage3.evaluate(scrollToBottom, scrollOptions);
+    // Test Page 3
+    const testPage3 = await browser.newPage();
+    await testPage3.goto(`${siteFull}/story/an-adrenaline-junkie-with-a-passion-for-filmmaking/`);
+    await new Promise(function (resolve) {
+        setTimeout(async function () {
+            await testPage3.evaluate(scrollToBottom, scrollOptions);
 
-      await percySnapshot(testPage3, 'Snapshot of test page 3', {
-        percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
-      });
-      resolve();
-    }, 3000);
-  });
-  
-  // Test Page 4
-  const testPage4 = await browser.newPage();
-  await testPage4.goto(
-    'https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/story/buoyed-by-hope-and-light-class-of-2024-departs-mayflower-hill/'
-  );
-  await new Promise(function (resolve) {
-    setTimeout(async function  () {
-      await testPage4.evaluate(scrollToBottom, scrollOptions);
+            await percySnapshot(testPage3, 'Snapshot of test page 3', {
+                percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; }`,
+            });
+            resolve();
+        }, 3000);
+    });
 
-      await percySnapshot(testPage4, 'Snapshot of test page 4', {
-        percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; } .wp-block-embed-youtube { .display: none; }`,
-      });
-      resolve();
-    }, 3000);
-  });
+    // Test Page 4
+    const testPage4 = await browser.newPage();
+    await testPage4.goto(
+        `${siteFull}/story/buoyed-by-hope-and-light-class-of-2024-departs-mayflower-hill/`
+    );
+    await new Promise(function (resolve) {
+        setTimeout(async function () {
+            await testPage4.evaluate(scrollToBottom, scrollOptions);
 
-  // Main Menu
-  const homePage = await browser.newPage();
-  await homePage.goto('https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site');
+            await percySnapshot(testPage4, 'Snapshot of test page 4', {
+                percyCSS: `.relatedSection { display:none; } .highlightsSection { display: none; } .read-time { display: none; } .wp-block-embed-youtube { .display: none; }`,
+            });
+            resolve();
+        }, 3000);
+    });
 
-  await homePage.waitForSelector('.open-menu');
-  await homePage.click('.open-menu');
+    // Main Menu
+    const homePage = await browser.newPage();
+    await homePage.goto(`${siteFull}/`);
 
-  const mainMenuSelector = '#main-menu';
-  await homePage.waitForSelector(mainMenuSelector);
+    await homePage.waitForSelector('.open-menu');
+    await homePage.click('.open-menu');
 
-  await percySnapshot(homePage, 'Snapshot of main menu', {
-    scope: mainMenuSelector,
-  });
+    const mainMenuSelector = '#main-menu';
+    await homePage.waitForSelector(mainMenuSelector);
 
-  // Contact Page
-  const contactPage = await browser.newPage();
+    await percySnapshot(homePage, 'Snapshot of main menu', {
+        scope: mainMenuSelector,
+    });
 
-  await contactPage.goto('https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/contact/');
+    // Contact Page
+    const contactPage = await browser.newPage();
 
-  await percySnapshot(contactPage, 'Snapshot of contact page');
+    await contactPage.goto(`${siteFull}/contact/`);
 
-  const resourcesPage = await browser.newPage();
+    await percySnapshot(contactPage, 'Snapshot of contact page');
 
-  await resourcesPage.goto('https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/resources-for-the-media/');
+    const resourcesPage = await browser.newPage();
 
-  await percySnapshot(resourcesPage, 'Snapshot of resources page');
+    await resourcesPage.goto(`${siteFull}/resources-for-the-media/`);
 
-  // Newsletter Page
-  const newsletterPage = await browser.newPage();
+    await percySnapshot(resourcesPage, 'Snapshot of resources page');
 
-  await newsletterPage.goto('https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/newsletter/');
+    // Newsletter Page
+    const newsletterPage = await browser.newPage();
 
-  await percySnapshot(newsletterPage, 'Snapshot of newsletter page');
+    await newsletterPage.goto(`${siteFull}/newsletter/`);
 
-  await browser.close();
+    await percySnapshot(newsletterPage, 'Snapshot of newsletter page');
+
+    await browser.close();
 })();
